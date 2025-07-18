f7c54a67d7d93c95f00288f3f437f7b3809bb6fb
Vladyslav Kuriachyi <vladlucky0@gmail.com>
2025-07-18 06:59:45 +0100

    18.07

--- js/JS.js
+++
@@ -1,7 +0,0 @@
-const burger = document.getElementById('burger');
-const nav = document.querySelector('.header_nav');
-
-burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    nav.classList.toggle('active');
-});
