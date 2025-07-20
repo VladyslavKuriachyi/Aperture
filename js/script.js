e6793289368e2e096a7ebf30ab9f7d333d08469c
Vladyslav Kuriachyi <vladlucky0@gmail.com>
2025-07-20 18:03:53 +0100

    20.07

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$( function() {
+    $( "#slider" ).slider();
+} );
+
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayTimeout: 1500,
+    autoplaySpeed: 1000,
+    loop: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1100:{
+            items:1
+        }
+    }
+
+})
+
+$( function() {
+    $( "#dialog" ).dialog();
+} );
